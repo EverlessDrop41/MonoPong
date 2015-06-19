517e76a65666fcccca0a4a84cd35ab0e1f393ce2
EverlessDrop41 <tomfperegrine@googlemail.com>
2015-06-19 19:43:07 +0100

    forgot file

---
+++ MonoPong/Objects/Bullets/Bullets.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+using MonoPong.Objects;
+
+namespace MonoPong.Objects
+{
+    class Bullets : List<Bullet>
+    {
+        public void CreateBullet(Rectangle rect, bool goingRight)
+        {
+            int dir = 5 * (goingRight ? 1 : -1);
+            this.Add(new Bullet(rect, dir ));
+        }
+    }
+}
