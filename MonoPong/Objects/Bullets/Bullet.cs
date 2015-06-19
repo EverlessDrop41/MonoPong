ff8ccb32380b9f17ecfdc4cdd0ca3f5c3eff4ee9
EverlessDrop41 <tomfperegrine@googlemail.com>
2015-06-19 19:42:44 +0100

    Began work on bullets

---
+++ MonoPong/Objects/Bullets/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MonoPong.Objects
+{
+    class Bullet : GameObject
+    {
+        public Bullet(Rectangle rect, int Speed)
+            : base(rect)
+        {
+            speed = Speed;   
+        }
+
+        public Bullet(Vector2 Position, Vector2 Size, int Speed)
+            : base(Position, Size)
+        {
+            speed = Speed;
+        }
+
+        int speed;
+
+        public override void Update(GameTime time)
+        {
+            this.Position.X += speed;
+            base.Update(time);
+        }
+    }
+}
